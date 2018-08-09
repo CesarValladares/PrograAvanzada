b5145801d2572b48b188c8d507eff76ce43501bc
CesarValladares <A01023506@itesm.mx>
2018-08-08 23:33:14 -0500

    clase 1

---
+++ type_sizes.c
@@ -0,0 +1,16 @@
+/* 
+    show the sizes of the data that can be stored in different variable types 
+
+    César Valladares
+    06/08/2018 
+*/
+
+#include <stdio.h>
+
+#define NUMBER 15
+
+int main (){
+    printf ("Hello %d\n", NUMBER);
+
+    return 0;
+}
