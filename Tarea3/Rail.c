90cfd9f0286245fa85d2ac23c941ee9cd5ffbfc4
CesarValladares <a01023506@itesm.mx>
2018-10-04 17:06:31 -0500

    windows

--- Tarea3/Rail.c
+++ Tarea3/Rail.c
@@ -1,37 +1,51 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-main()
+
+int main()
 {
-    int i,j,len,rails,count,code[100][1000];
+    int rails;
+    
+    int count= 0;
+    
+    char code[100][1000];
+
     char str[1000];
+
     printf("Enter a Secret Message\n");
-    gets(str);
-    len=strlen(str);
+    scanf("%[^\n]s", str);
+
+    int len=strlen(str);
+
     printf("Enter number of rails\n");
-    scanf("%d",&rails);
-    for(i=0;i<rails;i++){
-        for(j=0;j<len;j++){
+    scanf("%d", &rails);
+
+
+    for(int i = 0 ; i < rails ; i++){
+        for( int j = 0 ; j < len ; j++){
+
             code[i][j]=0;
         }
     }
-    count=0;
-    j=0;
-    while(j<len)
+
+    int j = 0;
+
+    while( j <len)
     {
         if(count%2==0)
         {
-            for(i=0;i<rails;i++){
-                //strcpy(code[i][j],str[j]);
-                code[i][j]=(int)str[j]; 
+            for(int i = 0 ; i <rails ; i++){
+                
+                code[i][j]=str[j]; 
+
                 j++;
             }   
  
         }
         else
         {   
-            for(i=rails-2;i>0;i--){
-                code[i][j]=(int)str[j];
+            for(int i = rails - 2 ; i > 0 ; i--){
+                code[i][j]=str[j];
                 j++;
             }  
         } 
@@ -39,11 +53,14 @@ main()
         count++;
     }
 
-    for(i=0;i<rails;i++){
-        for(j=0;j<len;j++){
+    for( int i = 0 ; i < rails ; i++){
+        for(j = 0 ; j < len ; j++){
+
             if(code[i][j]!=0)
                 printf("%c",code[i][j]);
         }
  
     }printf("\n");
+
+    return 0;
 }
